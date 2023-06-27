794a673937a29a847cd466d83735900868089924
Xenondq <almaz.eyupensar@gmail.com>
2023-06-27 15:11:22 +0100

    test

---
+++ android/app/src/main/kotlin/com/flutterjunction/homescreen_widget/HomeScreenWidgetProvider.kt
@@ -0,0 +1,32 @@
+package com.flutterjunction.homescreen_widget  // your package name
+
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.SharedPreferences
+import android.net.Uri
+import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetBackgroundIntent
+import es.antonborri.home_widget.HomeWidgetLaunchIntent
+import es.antonborri.home_widget.HomeWidgetProvider
+
+class HomeScreenWidgetProvider : HomeWidgetProvider() {
+     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray, widgetData: SharedPreferences) {
+        appWidgetIds.forEach { widgetId ->
+            val views = RemoteViews(context.packageName, R.layout.widget_layout).apply {
+
+                // Open App on Widget Click
+                val pendingIntent = HomeWidgetLaunchIntent.getActivity(context,
+                        MainActivity::class.java)
+                setOnClickPendingIntent(R.id.widget_root, pendingIntent)
+
+                val quote = widgetData.getString("_quote", null)
+
+
+
+                setTextViewText(R.id.word1, quote)
+
+            }
+            appWidgetManager.updateAppWidget(widgetId, views)
+        }
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:home_widget/home_widget.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+final List<String> dailyQuotes = [
+  "Good : İyi",
+  "Bad : Kötü",
+];
+
+String getDailyQuote() {
+  final DateTime now = DateTime.now();
+  final int index = now.day % dailyQuotes.length;
+  return dailyQuotes[index];
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  String quote = getDailyQuote();
+
+  @override
+  void initState() {
+    super.initState();
+    HomeWidget.widgetClicked.listen((Uri? uri) => loadData());
+    loadData();
+  }
+
+  void loadData() async {
+    await HomeWidget.saveWidgetData<String>('_quote', quote);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              quote,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
